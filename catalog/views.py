9fcfd5fc4eee4c2900aa41f8b1d94feea0359364
Ekhidna <ekhidna@disroot.org>
2025-11-10 21:09:47 +0700

    templates and views configured

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
-
+from django.views import generic
 def index(request):
     """
     Функция отображения для домашней страницы сайта.
@@ -19,3 +19,28 @@ def index(request):
         'index.html',
         context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors},
     )
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+
+    def get_context_data(self, **kwargs):
+        # В первую очередь получаем базовую реализацию контекста
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Добавляем новую переменную к контексту и инициализируем её некоторым значением
+        context['some_data'] = 'This is just some data'
+        return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'catalog/book_detail.html'
+
+class AuthorListView(generic.ListView):
+    model = Author
+    template_name = 'catalog/author_list.html'
+    context_object_name = 'author_list'
+    paginate_by = 10
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    template_name = 'catalog/author_detail.html'
