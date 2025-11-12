ca5efc98f246c9c2de6be3ad1ef7221a80d89218
ekhidna <ekhidna@disroot.org>
2025-11-12 10:23:32 +0700

    Register templates added to project directory

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
